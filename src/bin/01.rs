48240ad8bb8419eb0ff004a367869bbd64f39267
Olivia Isaacs <olivia@scaasic.com>
2023-12-01 22:50:30 -0600

    Day 1, Part 2: 10 MINUTES TO SPARE

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,11 @@
+use std::usize;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
     Some(
         input
-            .split('\n')
+            .lines()
             .map(|s| {
                 let mut digits = s.chars().filter_map(|c| c.to_digit(10));
                 let first = digits.next().unwrap_or_default();
@@ -18,8 +20,30 @@ pub fn part_one(input: &str) -> Option<u32> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(input.lines().map(parse_line).sum())
+}
+
+fn parse_line(line: &str) -> u32 {
+    let first = line
+        .char_indices()
+        .find_map(|(i, c)| c.to_digit(10).or(starts_with_digit_word(&line[i..])));
+    let last = line
+        .char_indices()
+        .rev()
+        .find_map(|(i, c)| c.to_digit(10).or(starts_with_digit_word(&line[i..])));
+
+    first.unwrap_or_default() * 10 + last.unwrap_or_default()
+}
+
+const DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+fn starts_with_digit_word(s: &str) -> Option<u32> {
+    DIGITS
+        .iter()
+        .enumerate()
+        .find_map(|(index, digit)| s.starts_with(digit).then_some(index as u32 + 1))
 }
 
 #[cfg(test)]
@@ -34,7 +58,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(281));
     }
 }
